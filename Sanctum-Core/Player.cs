12a8453188f23b97ebc7ef036f396051447f94bc
Gabe Righi <gaberighi@gmail.com>
2024-06-06 17:15:22 -0700

    Added player and playtable.

---
+++ Sanctum-Core/Player.cs
@@ -0,0 +1,71 @@
+﻿namespace Sanctum_Core
+{
+    using Sanctum_Core.CardClasses;
+    using Sanctum_Core.Networking;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public enum CardZone { Library, Graveyard, Exile, CommandZone, MainField, LeftField, RightField, Hand }
+    public class Player
+    {
+        public string Name { get; }
+        public string Uuid { get; }
+        public NetworkAttribute<string> DeckListRaw { get; }
+        public NetworkAttribute<int> Health { get; }
+        public NetworkAttribute<bool> ReadiedUp { get; set; }
+
+        public bool GameStarted { get; } = false;
+
+        private Dictionary<CardZone, CardContainerCollection> zoneToContainer = new Dictionary<CardZone, CardContainerCollection>();
+        public Player(string uuid, string name, int startingHealth, NetworkAttributeFactory networkAttributeFactory, CardFactory cardFactory)
+        {
+            this.Uuid = uuid;
+            this.Name = name;
+            Health = networkAttributeFactory.AddNetworkAttribute<int>(Uuid, startingHealth);
+            DeckListRaw = networkAttributeFactory.AddNetworkAttribute<string>(Uuid, "");
+            ReadiedUp = networkAttributeFactory.AddNetworkAttribute(Uuid, false);
+            InitializeBoards(networkAttributeFactory, cardFactory);
+        }
+
+        void InitializeBoards(NetworkAttributeFactory networkAttributeFactory, CardFactory cardFactory)
+        {
+            zoneToContainer[CardZone.Library] = new CardContainerCollection(CardZone.Library, this.Uuid, 1, null, networkAttributeFactory, cardFactory);
+            zoneToContainer[CardZone.Graveyard] = new CardContainerCollection(CardZone.Library, this.Uuid, 1, null, networkAttributeFactory, cardFactory);
+            zoneToContainer[CardZone.Exile] = new CardContainerCollection(CardZone.Library, this.Uuid, 1, null, networkAttributeFactory, cardFactory);
+            zoneToContainer[CardZone.CommandZone] = new CardContainerCollection(CardZone.Library, this.Uuid, 1, null, networkAttributeFactory, cardFactory);
+
+            zoneToContainer[CardZone.Hand] = new CardContainerCollection(CardZone.Library, this.Uuid, null, 1, networkAttributeFactory, cardFactory);
+
+            zoneToContainer[CardZone.MainField] = new CardContainerCollection(CardZone.Library, this.Uuid, null, 3, networkAttributeFactory, cardFactory);
+            zoneToContainer[CardZone.LeftField] = new CardContainerCollection(CardZone.Library, this.Uuid, null, 3, networkAttributeFactory, cardFactory);
+            zoneToContainer[CardZone.RightField] = new CardContainerCollection(CardZone.Library, this.Uuid, null, 3, networkAttributeFactory, cardFactory);
+
+        }
+
+        /// <summary>
+        /// Gets cardzone from player
+        /// </summary>
+        /// <param name="zone">Zone to get</param>
+        /// <returns>Requested zone</returns>
+        public CardContainerCollection GetCardContainer(CardZone zone)
+        {
+            return zoneToContainer[zone];
+        }
+
+        /// <summary>
+        /// Determines if the current user decklist is valid
+        /// </summary>
+        /// <returns>A list of the lines/names that were not able to be parsed</returns>
+        public List<string> ValidateDeckList(Func<string, bool> validateCardName)
+        {
+            (List<string> cardNames, List<string> errorLines) = CardParser.ParseDeckList(DeckListRaw.Value);
+            if (errorLines.Count > 0)
+            {
+                return errorLines;
+            }
+            cardNames = cardNames.Where(x => !validateCardName(x)).ToList();
+            return cardNames;
+        }
+    }
+}
---
+++ Sanctum-Core/Playtable.cs
@@ -0,0 +1,98 @@
+﻿namespace Sanctum_Core
+{
+    using Sanctum_Core.CardClasses;
+    using Sanctum_Core.Networking;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+
+    public class Playtable
+    {
+        private List<Player> _players = new List<Player>();
+
+        private NetworkManager _networkManager;
+
+        public NetworkAttribute<int> readyUpNeeded;
+
+        public bool GameStarted { get; set; } = false;
+
+        public event PropertyChangedEventHandler gameStarted = delegate { };
+
+        private CardFactory cardFactory;
+        private NetworkAttributeFactory networkAttributeFactory;
+
+        public Playtable(bool mock = false)
+        {
+            this.readyUpNeeded = new NetworkAttribute<int>("0", 4);
+            this.cardFactory = new CardFactory();
+            this.networkAttributeFactory = new NetworkAttributeFactory();
+            this.networkAttributeFactory.attributeValueChanged += _networkManager.NetworkAttributeChanged;
+            this._networkManager = new NetworkManager(networkAttributeFactory);
+
+        }
+
+        /// <summary>
+        /// Adds a player to the table
+        /// </summary>
+        /// <param name="name"> Name of the player</param>
+        /// <param name="uuid"> UUID of player</param>
+        public void AddPlayer(string name, string uuid)
+        {
+            if (GameStarted)
+            {
+                return;
+            }
+            Player player = new Player(uuid, name, 40, networkAttributeFactory, cardFactory);
+            player.ReadiedUp.valueChange += CheckForStartGame;
+            _players.Add(player);
+        }
+
+        /// <summary>
+        /// Gets a player in the game
+        /// </summary>
+        /// <param name="uuid">UUID of desired player</param>
+        /// <returns>Player or null depending of if uuid exists</returns>
+        public Player? GetPlayer(string uuid)
+        {
+            return _players.FirstOrDefault(player => player.Uuid == uuid); ;
+        }
+
+        void CheckForStartGame(object obj, PropertyChangedEventArgs args)
+        {
+            int readyCount = _players.Count(player => player.ReadiedUp.Value);
+            if (readyCount >= readyUpNeeded.Value)
+            {
+                StartGame();
+            }
+        }
+
+        void StartGame()
+        {
+            GameStarted = true;
+            SetupDecks();
+            gameStarted(this, new PropertyChangedEventArgs("Started"));
+        }
+
+        void SetupDecks()
+        {
+            foreach (Player player in _players)
+            {
+                (List<string> cardNames, _) = CardParser.ParseDeckList(player.DeckListRaw.Value);
+                CardContainerCollection library = player.GetCardContainer(CardZone.Library);
+                List<Card> cards = cardFactory.LoadCardNames(cardNames, networkAttributeFactory);
+                cards.ForEach(card => library.InsertCardIntoContainer(0, true, card, null, false));
+            }
+        }
+
+
+        public bool RemovePlayer(string uuid)
+        {
+            Player player = GetPlayer(uuid);
+            if (player != null)
+            {
+                return _players.Remove(player);
+            }
+            return false;
+        }
+    }
+}
