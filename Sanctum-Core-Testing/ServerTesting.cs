dc7d383a7a83e6c6685524572a5c86494440218b
Gabe Righi <gaberighi@gmail.com>
2024-06-17 19:49:26 -0700

    Added more tests for lobby. Changed how lobby is created.

--- Sanctum-Core-Testing/ServerTesting.cs
+++ Sanctum-Core-Testing/ServerTesting.cs
@@ -1,4 +1,5 @@
-﻿using Sanctum_Core;
+﻿using Newtonsoft.Json;
+using Sanctum_Core;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -48,9 +49,7 @@ namespace Sanctum_Core_Testing
             NetworkStream stream = client.GetStream();
             Server.SendMessage(stream, NetworkInstruction.CreateLobby, $"4");
             NetworkCommand? command = NetworkCommandManager.GetNextNetworkCommand(stream, new StringBuilder(), 4096);
-            Assert.IsNotNull(command);
-            Assert.That(command.instruction == "Must include name and lobby code");
-            Assert.That(command.opCode, Is.EqualTo((int)NetworkInstruction.InvalidCommand));
+            this.AssertCommandResults(command, NetworkInstruction.InvalidCommand, "Must include name and lobby code");
         }
 
         [Test]
@@ -61,9 +60,7 @@ namespace Sanctum_Core_Testing
             NetworkStream stream = client.GetStream();
             Server.SendMessage(stream, NetworkInstruction.CreateLobby, "Asd|Asd");
             NetworkCommand? command = NetworkCommandManager.GetNextNetworkCommand(stream, new StringBuilder(), 4096);
-            Assert.IsNotNull(command);
-            Assert.That(command.instruction == "Invalid lobby count");
-            Assert.That(command.opCode, Is.EqualTo((int)NetworkInstruction.InvalidCommand));
+            this.AssertCommandResults(command, NetworkInstruction.InvalidCommand, "Invalid lobby count");
         }
 
         [Test]
@@ -78,8 +75,7 @@ namespace Sanctum_Core_Testing
             client.Connect(IPAddress.Loopback, Server.portNumber);
             Server.SendMessage(client.GetStream(), NetworkInstruction.JoinLobby, $"{data[1]}|Gabe");
             command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096);
-            Assert.IsNotNull(command);
-            Assert.That(command.opCode, Is.EqualTo((int)NetworkInstruction.JoinLobby));
+            this.AssertCommandResults(command, NetworkInstruction.JoinLobby, null);
             Assert.That(command.instruction.Length, Is.EqualTo(this.uuidLength));
         }
 
@@ -91,9 +87,55 @@ namespace Sanctum_Core_Testing
             NetworkStream stream = client.GetStream();
             Server.SendMessage(stream, NetworkInstruction.JoinLobby, $"Spaghetti & Meatballs");
             NetworkCommand? command = NetworkCommandManager.GetNextNetworkCommand(stream, new StringBuilder(), 4096);
+            this.AssertCommandResults(command, NetworkInstruction.InvalidCommand, "Need to include Name and Lobby code");
+        }
+
+        [Test]
+        public void AddPlayerToLobbyTest()
+        {
+            TcpClient client = new();
+            client.Connect(IPAddress.Loopback, Server.portNumber);
+            Server.SendMessage(client.GetStream(), NetworkInstruction.CreateLobby, $"3|Gabriel");
+            NetworkCommand? command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096);
+            string[] data = command.instruction.Split('|');
+            client = new();
+            client.Connect(IPAddress.Loopback, Server.portNumber);
+            Server.SendMessage(client.GetStream(), NetworkInstruction.JoinLobby, $"{data[1]}|Gabe");
+            _ = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096); //  Skip Get UUID
+            command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096); // Should be a list of player names
+            this.AssertCommandResults(command, NetworkInstruction.PlayersInLobby, "[\"Gabe\",\"Gabriel\"]");
+        }
+
+        [Test]
+        public void StartLobbyTest()
+        {
+            TcpClient client = new();
+            client.Connect(IPAddress.Loopback, Server.portNumber);
+            Server.SendMessage(client.GetStream(), NetworkInstruction.CreateLobby, $"2|Gabriel");
+            NetworkCommand? command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096);
+            string[] data = command.instruction.Split('|');
+            string p1UUID = data[0];
+            client = new();
+            client.Connect(IPAddress.Loopback, Server.portNumber);
+            Server.SendMessage(client.GetStream(), NetworkInstruction.JoinLobby, $"{data[1]}|Gabe");
+            command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096); //  Skip Get UUID
+            data = command.instruction.Split('|');
+            string p2UUID = data[0];
+            command = NetworkCommandManager.GetNextNetworkCommand(client.GetStream(), new StringBuilder(), 4096); // Should be a start lobby call
+            this.AssertCommandResults(command, NetworkInstruction.StartGame, null);
+            Dictionary<string, string> expectedLobby = new() { { p1UUID, "Gabriel" } , { p2UUID , "Gabe"} };
+            Dictionary<string, string> actualDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(command.instruction);
+            CollectionAssert.AreEqual(expectedLobby, actualDictionary);
+        }
+
+        private void AssertCommandResults(NetworkCommand? command,NetworkInstruction expectedOpCode, string? expectedPayload)
+        {
             Assert.IsNotNull(command);
-            Assert.That(command.opCode, Is.EqualTo((int)NetworkInstruction.InvalidCommand));
-            Assert.That(command.instruction, Is.EqualTo("Need to include Name and Lobby code"));
+            Assert.That(command.opCode, Is.EqualTo((int)expectedOpCode));
+            if(expectedPayload is not null)
+            {
+                Assert.That(command.instruction, Is.EqualTo(expectedPayload));
+            }
         }
     }
 }
